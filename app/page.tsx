437cd1999386dc33a7aaaf64769e18c2cda34d28
Calvin <calvingsx@gmail.com>
2024-08-27 10:24:58 -0300

    Adicionando título na página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <main className="py-12">Conteúdo inicial</main>;
+  return (
+    <main className="py-12">
+      <div className="text-center space-y-4">
+        <h2 className="text-lg">MetroSP Status</h2>
+        <p>Acompanhe o <strong className="text-primary">status atualizado</strong> das linhas de metro de SP a cada 5 minutos</p>
+      </div>      
+    </main>
+  );
 }
