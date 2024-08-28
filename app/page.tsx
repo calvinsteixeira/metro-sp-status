2d3effb3ef5bb79bd47742b177022f24e9a066d2
Calvin <calvingsx@gmail.com>
2024-08-28 15:58:33 -0300

    Ajustando filtros

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,14 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { LineMetroCard, LinesDonutChart } from '@/components/index';
 
 //UTILS
+import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { LineMetroProps } from '@/components/containers/LineMetroCard';
 import { DonutChartProps } from '@/components/charts/LinesDonutChart';
 
 export default function Home() {
+  const [filterLineName, setFilterLineName] = React.useState<string>('');
+  const [filterStatusLine, setStatusLine] = React.useState<string>('all');
   const {
     isLoading,
     isError,
@@ -41,22 +44,33 @@ export default function Home() {
     normal: { label: 'Operação Normal', color: '#32a852' },
     reduced_speed: { label: 'Circulação de Trens', color: '#fc6603' },
     closed: { label: 'Operação encerrada', color: '#969696' },
-    paralyzed: { label: 'Operação paralizada', color: "#a83232" }
+    paralyzed: { label: 'Operação paralizada', color: '#a83232' },
   };
 
   const getCharData = (): DonutChartProps['chartData'] => {
-    const statuses: LineMetroProps['status'][] = ['normal', 'reduced_speed', 'closed', 'paralyzed']
-    const data: DonutChartProps['chartData'] = []
-    for(let status of statuses) {
+    const statuses: LineMetroProps['status'][] = ['normal', 'reduced_speed', 'closed', 'paralyzed'];
+    const data: DonutChartProps['chartData'] = [];
+    for (let status of statuses) {
       data.push({
         status: status,
-        availableLines: metroStatusData?.lines.filter(line => line.status == status).length || 0,
-        fill: chartConfig[status].color || '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')
-      })
+        availableLines: metroStatusData?.lines.filter((line) => line.status == status).length || 0,
+        fill:
+          chartConfig[status].color ||
+          '#' +
+            Math.floor(Math.random() * 16777215)
+              .toString(16)
+              .padStart(6, '0'),
+      });
     }
-    return data
-  } 
- 
+    return data;
+  };
+
+  const filteredLines = metroStatusData?.lines.filter(
+    (line) =>
+      (filterStatusLine === 'all' || line.status === filterStatusLine) &&
+      (line.name.toLowerCase().includes(filterLineName.toLowerCase()) ||
+       line.number.toLowerCase().includes(filterLineName.toLowerCase()))
+  );
 
   return (
     <main className="py-12">
@@ -69,15 +83,16 @@ export default function Home() {
       <div className="space-y-4">
         <div className="w-full space-y-2 mt-12">
           <Label htmlFor="buscar-linha">Buscar pelo nome/id</Label>
-          <Input id="buscar-linha" placeholder="informe o nome/id da linha" />
+          <Input id="buscar-linha" placeholder="informe o nome/id da linha" onChange={(e) => setFilterLineName(e.target.value)} />
         </div>
         <div className="space-y-2">
           <Label htmlFor="buscar-linha">Busque pelo status</Label>
-          <Select>
+          <Select onValueChange={setStatusLine} defaultValue={filterStatusLine}>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="status da linha" />
             </SelectTrigger>
             <SelectContent>
+              <SelectItem value="all">Todos os status</SelectItem>
               <SelectItem value="normal">Operação normal</SelectItem>
               <SelectItem value="reduced_speed">Circulação de trens</SelectItem>
               <SelectItem value="closed">Operação encerrada</SelectItem>
@@ -96,17 +111,17 @@ export default function Home() {
             <Skeleton className="w-full h-[8rem] rounded-md" />
           </div>
         ) : (
-          <div>
+          <div className="space-y-3">
             <LinesDonutChart updatedAt={metroStatusData?.updatedAt || null} chartConfig={chartConfig} chartData={getCharData()} />
-            {metroStatusData && metroStatusData.lines.length > 0 ? (
+            {metroStatusData && filteredLines && filteredLines.length > 0 ? (
               <div className="space-y-6">
-                {metroStatusData.lines.map((metroStatus: LineMetroProps, index: number) => (
+                {filteredLines.map((metroStatus: LineMetroProps, index: number) => (
                   <LineMetroCard key={index} {...metroStatus} updatedAt={metroStatusData.updatedAt} />
                 ))}
               </div>
             ) : (
-              <div>
-                <p>Nenhuma informação disponível</p>
+              <div className='mt-4'>
+                <p>Nenhuma linha foi encontrada</p>
               </div>
             )}
           </div>
