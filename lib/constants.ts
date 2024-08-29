ff94bf866e450c6845cc0d178c1c123e2e0af4de
Calvin <calvingsx@gmail.com>
2024-08-29 11:07:36 -0300

    Adicionando cores dos status das linhas de metro

---
+++ lib/constants.ts
@@ -0,0 +1,26 @@
+const metroStatusColors = {
+  normal: "#32a852",
+  reduced_speed: "#e0982b",
+  closed: "#969696",
+  paralyzed: "#a83232"
+}
+const metroStatusTwClassColors = {
+  normal: {
+    text: 'text-[#32a852]',
+    background: 'bg-[#32a852]'
+  },
+  reduced_speed: {
+    text: 'text-[#e0982b]',
+    background: 'bg-[#e0982b]'
+  },
+  closed: {
+    text: 'text-[#969696]',
+    background: 'bg-[#969696]'
+  },
+  paralyzed: {
+    text: 'text-[#a83232]',
+    background: 'bg-[#a83232]'
+  }
+}
+
+export { metroStatusColors, metroStatusTwClassColors }
