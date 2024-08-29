0ef7666e89ee942625d0919d3693455e22df1285
Calvin Teixeira <calvingsx@gmail.com>
2024-08-28 21:16:09 -0300

    Adicionando switch

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Switch } from '@/components/ui/switch';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { LineMetroCard, LinesDonutChart } from '@/components/index';
 
@@ -80,27 +81,33 @@ export default function Home() {
             Acompanhe de forma atualizada o <strong className="text-primary">status</strong> das linhas de metrô em SP.
           </p>
         </div>
-        <div className="flex flex-col sm:flex-row sm:items-center gap-3 mt-4">
-          <div>
-            <Label className="text-foreground" htmlFor="buscar-linha">
-              Buscar pelo nome/id
-            </Label>
-            <Input className='max-w-[20rem]' id="buscar-linha" placeholder="informe o nome/id da linha" onChange={(e) => setFilterLineName(e.target.value)} />
+        <div className="flex items-end justify-between">
+          <div className="flex flex-col sm:flex-row sm:items-center gap-3 mt-4">
+            <div>
+              <Label className="text-foreground" htmlFor="buscar-linha">
+                Buscar pelo nome/id
+              </Label>
+              <Input className="max-w-[20rem]" id="buscar-linha" placeholder="informe o nome/id da linha" onChange={(e) => setFilterLineName(e.target.value)} />
+            </div>
+            <div>
+              <Label htmlFor="buscar-linha">Busque pelo status</Label>
+              <Select onValueChange={setStatusLine} defaultValue={filterStatusLine}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="status da linha" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="all">Todos os status</SelectItem>
+                  <SelectItem value="normal">Operação normal</SelectItem>
+                  <SelectItem value="reduced_speed">Circulação de trens</SelectItem>
+                  <SelectItem value="closed">Operação encerrada</SelectItem>
+                  <SelectItem value="paralyzed">Operação paralizada</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
-          <div>
-            <Label htmlFor="buscar-linha">Busque pelo status</Label>
-            <Select onValueChange={setStatusLine} defaultValue={filterStatusLine}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="status da linha" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="all">Todos os status</SelectItem>
-                <SelectItem value="normal">Operação normal</SelectItem>
-                <SelectItem value="reduced_speed">Circulação de trens</SelectItem>
-                <SelectItem value="closed">Operação encerrada</SelectItem>
-                <SelectItem value="paralyzed">Operação paralizada</SelectItem>
-              </SelectContent>
-            </Select>
+          <div className='flex items-center gap-2'>
+            <Label>dark mode</Label>
+            <Switch />
           </div>
         </div>
         <div className="mt-6">
