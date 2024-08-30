508142b5034f821c4207a0502fbcad9ba623400f
Calvin <calvingsx@gmail.com>
2024-08-30 17:23:28 -0300

    Ajustando função de data e horário para usar o modelo de 24h

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,12 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatDateBR(isoString: string) {
   const [datePart, timePart] = isoString.split('T');
   const [year, month, day] = datePart.split('-');
-  let [hour, minute] = timePart.split(':');
+  const [hour, minute] = timePart.split(':');
 
-  let hourInt = parseInt(hour, 10);
-  const period = hourInt >= 12 ? 'pm' : 'am';
-  hourInt = hourInt % 12 || 12;
-  hour = hourInt.toString().padStart(2, '0');
-
-  return `${day}/${month}/${year} às ${hour}:${minute} ${period}`;
+  return `${day}/${month}/${year} às ${hour}:${minute}`;
 }
